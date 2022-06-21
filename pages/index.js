845f4f8c2ef02c8bbd2eda624c3ec26dac36abcc
Piotr <piotr.bania@europe.com>
2022-06-21 18:49:21 +0100

    init

---
+++ pages/index.js
@@ -0,0 +1,82 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+return (
+<div>
+
+	<Head>
+		<title>Paul Financial Consulting</title>
+		<meta name="description" content="Paul Financial Consulting" />
+		<link rel="icon" href="/favicon.ico" />
+	</Head>
+
+	<header>
+		<div className="parent">
+			<Image className="logo" src="/logoPFC.svg" alt="logo" layout="fill" />
+			
+			<li className="menu">
+				<a href="#">Home</a>
+				<a href="#">Services</a>
+				<a href="#">Tax</a>
+				<a href="#">Useful Links</a>
+				<a href="#">News</a>
+				<a href="#">Contact</a>
+			</li>
+
+			<button className="button">Button</button>
+		</div>
+	</header>
+
+	<main>
+		<h1>
+			Welcome to Paul Financial Consulting.
+		</h1>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam minus dolore qui, laboriosam nisi earum
+			dolor repudiandae sunt alias quos quam? Facere quas cupiditate beatae sunt labore perspiciatis minima animi
+			accusamus quae officiis eaque, aperiam dicta dolores laudantium modi. Quae amet, commodi vero provident
+			officiis ut blanditiis fuga eum distinctio maiores expedita. Voluptas similique ipsam aliquid delectus ex
+			quo accusamus! Quo aliquid esse sunt neque, magni assumenda rem, laboriosam quasi dolores facere, quia
+			deleniti expedita optio laudantium? Ipsa voluptatum tempore nihil itaque optio dicta, ex quidem! Autem
+			quisquam repudiandae dolore ad asperiores nihil tenetur neque animi eius? Nesciunt excepturi quas quos iste
+			eum! Illo, accusamus obcaecati soluta similique deleniti libero ducimus unde voluptas molestiae! Repellendus
+			reiciendis ipsam mollitia a molestiae alias voluptatum illo earum aliquid, ad deserunt dolor consequuntur et
+			laudantium illum suscipit quis quos praesentium corporis eaque eligendi debitis unde molestias eveniet.
+			Neque odio dolores eos placeat quos quaerat totam, commodi eum a sint magnam laborum molestias eligendi,
+			officiis culpa doloribus voluptates, voluptatibus nostrum reprehenderit eaque beatae perspiciatis.
+			Reprehenderit, nostrum necessitatibus excepturi eligendi iste exercitationem sint totam rem eveniet laborum
+			temporibus est assumenda fuga magni odit autem repellendus aliquam culpa nulla dolorem illo vero nam neque.
+			Perspiciatis, nostrum. Sunt ipsum veniam tempore natus debitis dicta error quisquam, fugiat doloremque
+			delectus totam nesciunt. Id libero atque, quibusdam vitae amet dolores temporibus! Ab labore ullam nam
+			voluptatibus dolorem eum dolor non sequi? Modi, quos velit quaerat repudiandae reiciendis eum ab
+			consequuntur odio. Soluta eaque, sint hic laborum dolor ducimus tempore officiis sapiente aliquam ut sunt
+			nam explicabo quo consequuntur a distinctio eum harum! Praesentium fuga delectus deserunt dicta architecto
+			et. Atque alias dolor nostrum itaque ipsum id doloribus fugit minus, maxime minima, officiis voluptatem.
+			Maiores accusantium doloribus repellendus reprehenderit aspernatur deleniti officia illum consectetur
+			deserunt quo, et nostrum ipsa excepturi necessitatibus pariatur labore amet animi doloremque explicabo eius
+			nulla atque aperiam velit natus. Quaerat voluptatem maxime temporibus sequi consequuntur. Fugit omnis natus
+			iure dolore cupiditate ab excepturi ex asperiores, vero soluta qui. Sit reprehenderit incidunt totam
+			quisquam sed nulla delectus deleniti tempora vitae, a recusandae distinctio repudiandae, ea accusantium.
+			Recusandae autem ipsum maiores enim aperiam similique, saepe non amet suscipit excepturi, maxime expedita
+			veritatis iusto voluptate eos velit? Cumque labore est saepe. Nulla neque itaque iusto quos dicta impedit
+			atque deserunt, tempora quibusdam soluta repudiandae sit rerum reprehenderit numquam fugiat repellat, enim
+			accusantium aperiam. Expedita ducimus commodi eaque, laudantium ipsam temporibus a nulla quas maxime ipsa.
+			Aliquam ipsam laboriosam non voluptatum? Aliquid quasi, perferendis doloremque reiciendis beatae molestias
+			cupiditate incidunt blanditiis placeat culpa voluptate doloribus atque cum, dolorum asperiores et
+			consequatur provident. Similique aliquid nam necessitatibus, et id provident incidunt exercitationem dolorum
+			impedit excepturi deleniti, dignissimos perspiciatis maiores vitae minima saepe optio ipsum dolor, vero
+			consectetur beatae accusamus tenetur labore veniam? A reprehenderit odit atque sunt placeat, itaque fugiat
+			ex at, excepturi asperiores aperiam. Ratione corporis eos quis earum! Cumque, rem. Ab possimus quis tempore
+			fuga at facilis ea quaerat quibusdam suscipit nesciunt, quasi facere necessitatibus qui, dicta obcaecati
+			dolore accusamus?
+		</p>
+	</main>
+
+	<footer>
+
+	</footer>
+</div>
+)
+}
